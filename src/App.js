1d71872bbb5f9d6d02408cb0f0faa66bad3d9b2c
trdp30 <trdp30@gmail.com>
2021-05-06 10:14:22 +0530

    firebase integrated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import "./App.less";
 import store from "./store";
 import { Provider } from "react-redux";
 import Navigations from "./navigations";
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCcmNhTBGgiA-zklWpGGxe1NRRgYOkcwnA",
+  authDomain: "find-out-405a8.firebaseapp.com",
+  databaseURL: "https://find-out-405a8.firebaseio.com",
+  projectId: "find-out-405a8",
+  storageBucket: "find-out-405a8.appspot.com",
+  messagingSenderId: "306597878774",
+  appId: "1:306597878774:web:36d40b793f755a414a85d7",
+  measurementId: "G-KLKTYHRX97"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
   return (

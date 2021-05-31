59f24d65d390aba36f646bdb9b64439e36b35425
Sailab12345 <sailabbasak1998@gmail.com>
2021-05-31 19:18:35 +0530

    login made responsive

--- src/screens/authentication/login.js
+++ src/screens/authentication/login.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { authenticateInitiate } from "../../store/actions/session.action";
+//import pic from "../../../public/assests/images/open.jpeg";
 
 const Login = (props) => {
   const { triggerLogin, history, session } = props;
@@ -38,92 +39,98 @@ const Login = (props) => {
 
   return (
     <>
-      <div
-        className="ui raised segment align-center"
-        style={{ height: "auto", margin: "10vh", background: "lightgray" }}>
-        <div className="ui raised segment" style={{ margin: "5vh" }}>
-          <div className="ui two column grid">
-            <div className="nine wide column">
-              <img src="public/assets/icons/home.png" alt=""></img>
-            </div>
-            <div className="seven wide column ">
-              <div className="ui equal width grid margin-no">
-                <div className="row">
-                  <div className="column center aligned ">
-                    <span className="cursor-pointer text-color-positive">Login</span>
-                  </div>
-                  <div className="column center aligned ">
-                    <span className="cursor-pointer text-color-positive" onClick={handleRegister}>
-                      Register
-                    </span>
-                  </div>
-                </div>
-
-                <div className="row" style={{ marginTop: "10vh" }}>
-                  <div className="column">
-                    <div className=" text-size-large text-color-black text-weight-medium margin-no">
-                      <h3> Welcome to Login Page </h3>
+      <div className="ui grid container" style={{ margin: "7vh" }}>
+        <div
+          className="ui raised segment align-center fit-content"
+          style={{ background: "lightgray", width: "100%", padding: "4vh", height: "fit-content" }}>
+          <div
+            className="ui raised segment login-segment"
+            style={{ height: "100%", width: "100%" }}>
+            <div className="ui two column  grid">
+              <div className="nine wide computer only column">
+                <img src="" />
+              </div>
+              <div className=" seven wide computer sixteen wide mobile column">
+                <div class=" ui  grid ">
+                  <div class="doubling four column row">
+                    <div class="left floated column">
+                      <div
+                        className="cursor-pointer text-color-positive"
+                        style={{ marginLeft: "3vh" }}>
+                        Login
+                      </div>
+                    </div>
+                    <div class="right floated column">
+                      <div
+                        className="cursor-pointer text-color-positive"
+                        style={{ marginRight: "3vh" }}
+                        onClick={handleRegister}>
+                        Register
+                      </div>
                     </div>
                   </div>
-                </div>
-
-                <div className="row" style={{ marginTop: "2vh", paddingBottom: 0 }}>
-                  <div className="column">
-                    <div className=" text-size-medium text-color-black text-weight-medium margin-no">
-                      Email address
+                  <div className="doubling two column row" style={{ marginTop: "10vh" }}>
+                    <div className="column">
+                      <div className=" text-size-large text-color-black text-weight-medium margin-no">
+                        <h3> Welcome to Login Page </h3>
+                      </div>
                     </div>
                   </div>
-                </div>
-
-                <div className="row ">
-                  <div className="twelve wide column ">
-                    <div className="ui input focus" style={{ width: "100%" }}>
-                      <input
-                        type="email"
-                        name="email"
-                        onChange={updateState}
-                        placeholder="Your email address..."
-                      />
+                  <div className="doubling two column row" style={{ marginTop: "2vh" }}>
+                    <div className="column">
+                      <div className=" text-size-medium text-color-black text-weight-medium margin-no">
+                        Email address
+                      </div>
                     </div>
                   </div>
-                </div>
 
-                <div className="row" style={{ marginTop: "1vh", paddingBottom: 0 }}>
-                  <div className="column">
-                    <div className=" text-size-medium text-color-black text-weight-medium margin-no">
-                      Password
+                  <div className=" doubling two column row">
+                    <div className="ten wide column">
+                      <div className="ui input focus" style={{ width: "100%" }}>
+                        <input
+                          type="email"
+                          name="email"
+                          onChange={updateState}
+                          placeholder="Your email address..."
+                        />
+                      </div>
                     </div>
                   </div>
-                </div>
-
-                <div className="row ">
-                  <div className="twelve wide column ">
-                    <div className="ui input focus" style={{ width: "100%" }}>
-                      <input
-                        type="password"
-                        name="password"
-                        onChange={updateState}
-                        placeholder="Enter your Password"
-                      />
+                  <div className="doubling two column row" style={{ marginTop: "2vh" }}>
+                    <div className="column">
+                      <div className=" text-size-medium text-color-black text-weight-medium margin-no">
+                        Password
+                      </div>
                     </div>
                   </div>
-                </div>
-
-                <div
-                  className="five wide column"
-                  style={{ paddingRight: "0%", width: "fit-content" }}>
-                  <div className=" text-size-medium text-color-secondary text-weight-medium margin-no padding-no">
-                    <a href="www.wikipedia.com">Forgot Password ? </a>
+                  <div className=" doubling two column row">
+                    <div className=" ten wide column">
+                      <div className="ui input focus" style={{ width: "100%" }}>
+                        <input
+                          type="password"
+                          name="password"
+                          onChange={updateState}
+                          placeholder="Enter your Password"
+                        />
+                      </div>
+                    </div>
                   </div>
-                </div>
-                <div className="row " style={{ paddingBottom: 0 }}>
-                  <div className="twelve wide column ">
-                    <button
-                      className="ui button"
-                      style={{ width: "100%", marginBottom: "5vh" }}
-                      onClick={handleLogin}>
-                      LOGIN
-                    </button>
+                  <div className="doubling two column row">
+                    <div className="column">
+                      <div className=" text-size-medium text-color-secondary text-weight-medium margin-no padding-no">
+                        <a href="www.wikipedia.com">Forgot Password? </a>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="doubling two column row">
+                    <div className="ten wide column">
+                      <button
+                        className="ui button"
+                        style={{ width: "100%", marginBottom: "5vh" }}
+                        onClick={handleLogin}>
+                        LOGIN
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
